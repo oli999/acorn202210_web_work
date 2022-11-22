6cdb1e7f9995964a92d3f5dfd4903a59099e8e4b
kimgura <oli999@naver.com>
2022-11-22 11:44:10 +0900

    Step04 에  비밀번호 수정 기능 구현함

--- Step04_Final/src/main/java/test/users/dao/UsersDao.java
+++ Step04_Final/src/main/java/test/users/dao/UsersDao.java
@@ -20,6 +20,41 @@ public class UsersDao {
 		return dao;
 	}
 	
+	//비밀 번호를 수정하는 메소드
+	public boolean updatePwd(UsersDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "UPDATE users"
+					+ " SET pwd=?"
+					+ " WHERE id=? AND pwd=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 바인딩할게 있으면 해주고
+			pstmt.setString(1, dto.getNewPwd());
+			pstmt.setString(2, dto.getId());
+			pstmt.setString(3, dto.getPwd());
+			// INSERT OR UPDATE OR DELETE 문을 수행하고 수정되거나, 삭제되거나, 추가된 ROW 의 갯수 리턴 받기
+			rowCount = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (rowCount > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	//인자로 전달된 아이디에 해당하는 가입정보를 리턴해주는 메소드
 	public UsersDto getData(String id) {
 		
